9ea805f151684a5dfc3aad2d0e03369be0c69bc9
techstreak <135229687+techstreak@users.noreply.github.com>
2024-09-02 11:46:40 +0530

    Update settings.py

--- ecommercebackend/ecommercebackend/settings.py
+++ ecommercebackend/ecommercebackend/settings.py
@@ -35,7 +35,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thepagedoor@gmail.com'
-EMAIL_HOST_PASSWORD = 'tmji jkgq agkb poqt'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxxxx'
 
 
 # Application definition
